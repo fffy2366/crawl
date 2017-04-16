5b6f96e8953ec5f0f64b086c7f9fe66547957f55
Frank <fengxuting@gmail.com>
2017-04-16 22:37:06 +0800

    shell

--- bin/crawl_bjjs.sh
+++ bin/crawl_bjjs.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #sh bin/crawl_bjjs.sh
 #chmod -R 755 /opt/www/crawl/bin/crawl_bjjs.sh
-#0 1/* * * *  /opt/www/crawl/bin/crawl.sh >> /tmp/crawl_bjjs.log 2>&1
+#0 */1 * * *  /opt/www/crawl/bin/crawl_bjjs.sh >> /tmp/crawl_bjjs.log 2>&1
 # crontab -e
 basepath=$(cd `dirname $0`; pwd)
 project_dir=$(dirname $basepath)
