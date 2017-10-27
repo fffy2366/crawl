a382be5e28bff254a679a3e7ea9006325a2cda31
frank <fengxuting@qq.com>
2017-10-27 13:44:21 +0800

    Update fang

--- tutorial/tutorial/spiders/bjjs_spider.py
+++ tutorial/tutorial/spiders/bjjs_spider.py
@@ -15,12 +15,12 @@ class BjjsSpider(scrapy.Spider):
     name = "bjjs"
     allowed_domains = ["www.bjjs.gov.cn"]
     start_urls = (
-        'http://www.bjjs.gov.cn/bjjs/fwgl/zzxspzf/index.shtml',
+        'http://www.bjjs.gov.cn/bjjs/xxgk/ztzl/gycqzf/index.shtml',
     )
     domains = "http://www.bjjs.gov.cn"
     def parse(self, response):
         print("crwwl start:--------------------------------------->")
-        lis = response.xpath('//div[@class="tzgg_list_box"]/ul/li')
+        lis = response.xpath('//div[@id="tzgg1"]/ul[@class="tzgg_list_box"]/li')
         for index,li in enumerate(lis):
             if index>0:
                 break
@@ -35,12 +35,12 @@ class BjjsSpider(scrapy.Spider):
                 # 邮件通知
                 print "email notice"
                 email = Email()
-                link = "<a href=\"http://www.bjjs.gov.cn/bjjs/fwgl/zzxspzf/index.shtml\">查看</a>"
+                link = "<a href=\"http://www.bjjs.gov.cn/bjjs/xxgk/ztzl/gycqzf/index.shtml\">查看</a>"
                 content  = title+link
                 content = u''.join(content).encode('utf-8').strip()
                 # Todo:1.字符编码√
                 # Todo:2.发送html内容√
-                if email.send_mail(["fengxuting@qq.com"],"自住型商品房内容更新了",content):
+                if email.send_mail(["fengxuting@qq.com"],"共有产权房内容更新了",content):
                     print "send success"
                 else:
                     print "send fail"
